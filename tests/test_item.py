077713ed916332146b19dc0ad7e345be9c3e67c2
German <germanstarinenko@gmail.com>
2023-04-04 23:14:27 +0300

    complite

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price():
+    with pytest.raises(AssertionError):
+        assert item1.price == float
+
+def test_apply_discount():
+    assert item1.apply_discount() == None
