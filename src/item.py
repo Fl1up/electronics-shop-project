37c5f447c4030acc08c09d0093a676ed066b551c
German <germanstarinenko@gmail.com>
2023-04-07 15:03:16 +0300

    complite

--- src/item.py
+++ src/item.py
@@ -1,16 +1,13 @@
+import csv
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        self.all = self.all.append(f"{Item.__repr__(self)}")
 
     def calculate_total_price(self) -> float:
         return self.price * self.quantity
@@ -18,5 +15,26 @@ class Item:
 
     def apply_discount(self) -> None:
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return f"{self.__name}"
+
+    @name.setter
+    def name(self,word):
+        name = word.split(":")
+        for i in name:
+            self.__name = i
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                cls.all.append(cls(row["name"], row["price"], row["quantity"]))
+
+    @staticmethod
+    def string_to_number(x):
+        return int(float(x))
 
 
