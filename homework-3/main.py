55cd42e74ce0022a1889249166abbd3ed02a688f
German <germanstarinenko@gmail.com>
2023-04-11 10:29:26 +0300

    complite

--- homework-3/main.py
+++ homework-3/main.py
@@ -2,7 +2,5 @@ from src.item import Item
 
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
-    print(repr(item1) == "Item('Смартфон', 10000, 20)")
-    print(type(repr(item1)))
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
     assert str(item1) == 'Смартфон'
