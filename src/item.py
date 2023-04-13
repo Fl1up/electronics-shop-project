915bd093e9a629b383d0152af62e560211e0b1e1
German <germanstarinenko@gmail.com>
2023-04-13 22:08:54 +0300

    complite

--- src/item.py
+++ src/item.py
@@ -1,7 +1,6 @@
 import csv
 
 
-
 class Item:
     pay_rate = 1.0
     all = []
@@ -11,10 +10,8 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-
     def calculate_total_price(self) -> float:
         return self.price * self.quantity
-
 
     def apply_discount(self) -> None:
         self.price = self.price * self.pay_rate
@@ -24,21 +21,18 @@ class Item:
         return self.__name
 
     @name.setter
-    def name(self,word):
+    def name(self, word):
         names = word.split(":")
         for name in names:
             if len(name) <= 10:
                 self.__name = name
-            else:
-                return f" Exception: Длина наименования товара превышает 10 символов"
 
     @classmethod
     def instantiate_from_csv(cls):
-        with open('../src/items.csv',"r" ,encoding='UTF-8') as f:
+        with open('../src/items.csv', "r", encoding='UTF-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                if len(row["name"]) <= 10:
-                    cls.all.append(cls(row["name"], row["price"], row["quantity"]))
+                cls.all.append(cls(row["name"], row["price"], row["quantity"]))
 
     @staticmethod
     def string_to_number(x):
@@ -46,7 +40,6 @@ class Item:
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
-
 
     def __str__(self):
         return f'{self.__name}'
