80ffeb41ec50397c6986bcfce3ffc574165c4274
German <germanstarinenko@gmail.com>
2023-04-30 12:30:45 +0300

    complite

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,31 +3,23 @@ from src.item import Item
 
 class MixinLog:
     def __init__(self):
-        self.language = "EN"
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
 
     def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
+        if self.__language == "EN":
+            self.__language = "RU"
             return self
-        elif self.language == "RU":
-            self.language = "EN"
+        else:
+            self.__language = "EN"
             return self
 
 
 class KeyBoard(Item, MixinLog):
-    def __init__(self, name, price, quantity, language='EN'):
-        super().__init__(name, price, quantity)
-        self._language = language
-
-    @property
-    def language(self):
-        return self._language
-
-    @language.setter
-    def language(self, value):
-        if value not in ["EN", "RU"]:
-            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
-        self._language = value
+    pass
 
 
 
